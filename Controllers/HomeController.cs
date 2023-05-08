781d7e5cc2a78e9e01f9d5176144e8aa3b215090
ufaruki <usman.faruki@gmail.com>
2023-05-07 21:19:24 -0500

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moneris_Hosted_Paypage_Demo.Models;
+using System.Diagnostics;
+
+namespace Moneris_Hosted_Paypage_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.StoreId = _configuration["MonerisHostedPaypage:StoreId"];
+            ViewBag.Key = _configuration["MonerisHostedPaypage:Key"];
+            ViewBag.Url = _configuration["MonerisHostedPaypage:Url"];
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Moneris_Hosted_Paypage_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
